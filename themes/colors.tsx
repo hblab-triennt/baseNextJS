7156bec317c1565b35eda41c7e77a96122c89ce1
hblab-triennt <triennt@hblab.vn>
2022-03-24 10:04:06 +0700

    create base NextJS

---
+++ themes/colors.tsx
@@ -0,0 +1,16 @@
+/**
+ * Base Colors
+ *
+ * Text Colors
+ * - Suffix 'Text'
+ *
+ * Others
+ */
+
+const colors = {
+    // BaseColors
+    primary: '#69CA6D',
+    /** add colors here*/
+}
+
+export default colors
---
+++ themes/images.tsx
@@ -0,0 +1,12 @@
+// add image in project
+const IMAGE_PATH = '../..'
+/** here is path of image folder */
+const iconDemo = `${IMAGE_PATH}something...`
+/** here is path of image */
+
+const images = {
+    iconDemo,
+}
+/** create object image */
+export default images
+/** export object image */
